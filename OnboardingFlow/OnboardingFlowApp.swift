4cad5a1e0f34f50f52e5796dc185e3c0bf7dac8a
candyap <candyap182@icloud.com>
2024-07-30 13:30:45 +0700

    Initial Commit

---
+++ OnboardingFlow/OnboardingFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnboardingFlowApp.swift
+//  OnboardingFlow
+//
+//  Created by Candy Pangestu on 7/30/24.
+//
+
+import SwiftUI
+
+@main
+struct OnboardingFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ OnboardingFlowTests/OnboardingFlowTests.swift
@@ -0,0 +1,17 @@
+//
+//  OnboardingFlowTests.swift
+//  OnboardingFlowTests
+//
+//  Created by Candy Pangestu on 7/30/24.
+//
+
+import Testing
+@testable import OnboardingFlow
+
+struct OnboardingFlowTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
