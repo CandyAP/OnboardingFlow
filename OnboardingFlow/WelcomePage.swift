a66b1c204457962ad29c99768252197be8d2e021
candyap <candyap182@icloud.com>
2024-07-30 13:53:40 +0700

    add welcome page

---
+++ OnboardingFlow/WelcomePage.swift
@@ -0,0 +1,20 @@
+//
+//  WelcomePage.swift
+//  OnboardingFlow
+//
+//  Created by Candy Pangestu on 7/30/24.
+//
+
+import SwiftUI
+
+struct WelcomePage: View {
+    var body: some View {
+        Text("Welcome to MyApp")
+            .font(.title)
+            .fontWeight(.semibold)
+    }
+}
+
+#Preview {
+    WelcomePage()
+}
