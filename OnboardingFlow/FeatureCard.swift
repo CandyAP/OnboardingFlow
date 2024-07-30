bf02a104846f66f0ce20fb4b856ec6c319d1372c
candyap <candyap182@icloud.com>
2024-07-30 16:50:29 +0700

    add feature card

---
+++ OnboardingFlow/FeatureCard.swift
@@ -0,0 +1,30 @@
+//
+//  FeatureCard.swift
+//  OnboardingFlow
+//
+//  Created by Candy Pangestu on 7/30/24.
+//
+
+import SwiftUI
+
+struct FeatureCard: View {
+    let iconName: String
+    let description: String
+    
+    var body: some View {
+        HStack {
+            Image(systemName: iconName)
+            
+            Text(description)
+            
+            Spacer()
+        }
+        .padding()
+        .background(.tint, in: RoundedRectangle(cornerRadius: 12))
+        .foregroundColor(.white)
+    }
+}
+
+#Preview {
+    FeatureCard(iconName: "person.2.crop.square.stack.fill", description: "A multiline description about a feature paired with the image on the left.")
+}
