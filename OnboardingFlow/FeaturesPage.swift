984581ba53f2ba5451bd8e7ebd9fe6c85e158585
candyap <candyap182@icloud.com>
2024-07-30 14:08:57 +0700

    add features page

---
+++ OnboardingFlow/FeaturesPage.swift
@@ -0,0 +1,22 @@
+//
+//  FeaturesPage.swift
+//  OnboardingFlow
+//
+//  Created by Candy Pangestu on 7/30/24.
+//
+
+import SwiftUI
+
+struct FeaturesPage: View {
+    var body: some View {
+        VStack {
+            Text("Features")
+                .font(.title)
+                .fontWeight(.semibold)
+        }
+    }
+}
+
+#Preview {
+    FeaturesPage()
+}
